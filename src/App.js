a16dedc1ab9e69c119c3dc690b2fa141cdb12fe4
Sudhanshu ogale <sudhanshuogale@gmail.com>
2023-04-28 18:17:20 +0530

    change

--- src/App.js
+++ src/App.js
@@ -13,6 +13,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/cart" element={<Cart />} />
+        <h1>Sudhanshu</h1>
       </Routes>
       <Toaster />
     </BrowserRouter>
